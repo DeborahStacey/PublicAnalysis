f54b9551abb16f73b99ca81fa5168cee3713f817
Erik Zorn - Wallentin <ezornwal@ultron.socs.uoguelph.ca>
2016-10-25 10:08:33 -0400

    json object working

--- src/js/userInteraction.js
+++ src/js/userInteraction.js
@@ -25,24 +25,20 @@
 		document.getElementById('2').value = "1";
 	}
 	
-	function generate(){
-		alert("Generate...");
-		var txtFile = "test.txt";
-   		var file = new File(txtFile,"write");
-   		var str = JSON.stringify({ "question": document.getElementById('question').value, "region": document.getElementById('region').value });
-
-   		file.open(); 
-   		file.writeline(str);
-   		file.close();
-   		alert("Done!");
-	}
-	
-	
 	/*-------
 		Purpose: Testing Jasmine Framework
 		Parameters: NONE
 		Return: "Hello world!"
 	-------*/
-	function helloWorld() {
-		return "Hello world!";
+	function generate(){
+		
+		// Setup JSON object based on the input from the user on the interface
+		var input =	'{ "interface" : [' +
+					'{ "question":"' + document.getElementById('question').value + '" , "region":"'+ document.getElementById('region').value +'" , "catBreed":"'+ document.getElementById('catBreed').value +'" , "age":"'+ document.getElementById('age').value +'" , "weight":"'+ document.getElementById('weight').value +'" , "gender":"'+ document.getElementById('gender').value +'" , "height":"'+ document.getElementById('height').value +'" } ]}';
+					
+		// Convert from String to JSON Object
+   		var jsonObj = JSON.parse(input);
+		
+		// Display JSON object on the screen 
+		document.getElementById('showResult2').innerHTML = jsonObj.interface[0].question + " " + jsonObj.interface[0].region + " " + jsonObj.interface[0].catBreed + " " + jsonObj.interface[0].age + " " + jsonObj.interface[0].weight + " " + jsonObj.interface[0].gender + " " + jsonObj.interface[0].height;
 	}
