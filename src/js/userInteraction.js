eba54cf163d6dc437abb996381cdee1072e1b260
Erik Zorn - Wallentin <ezornwal@ultron.socs.uoguelph.ca>
2016-10-25 11:21:59 -0400

    error checking 1

--- src/js/userInteraction.js
+++ src/js/userInteraction.js
@@ -13,7 +13,7 @@
 		Return: NONE
 	-------*/
 	function reset () {
-		document.getElementById('question').value = "question0";
+		document.getElementById('question').value = "";
 		document.getElementById('region').value = "region0";
 		document.getElementById('catBreed').value = "catBreed0";
 		document.getElementById('age').value = "age0";
@@ -21,8 +21,10 @@
 		document.getElementById('gender').value = "gender0";
 		document.getElementById('height').value = "height0";
 		document.getElementById('showResult2').innerHTML = "";
-		document.getElementById('1').value = "1";
-		document.getElementById('2').value = "1";
+	}
+	
+	function isEmpty(str) {
+    	return !str || !/[^\s]+/.test(str);
 	}
 	
 	/*-------
@@ -31,14 +33,42 @@
 		Return: "Hello world!"
 	-------*/
 	function generate(){
+		// Reset Display - Demo purposes only
+		document.getElementById('showResult2').innerHTML = "";
+	
+		// Get the slot of the question selected
+		var shownVal = document.getElementById("question").value;
+		alert(shownVal);
+		
+		// Catch any input in the question box that is not empty or a valid question and throw an error
+		try {
+   			var questionSelected = document.querySelector("#questions option[value='"+shownVal+"']").dataset.value;
+		}
+		catch (e) {
+   			alert("Please enter a question from the drop-down menu.");
+			return;
+		}
+		
+		// Catch any input in the question box that is not empty or a valid question and throw an error
+		//var questionSelected = document.querySelector("#questions option[value='"+shownVal+"']").dataset.value;
+		//if (questionSelected == null){
+			//alert("Please enter a question from the drop-down menu.");
+			//return;
+		//}
 		
 		// Setup JSON object based on the input from the user on the interface
 		var input =	'{ "interface" : [' +
-					'{ "question":"' + document.getElementById('question').value + '" , "region":"'+ document.getElementById('region').value +'" , "catBreed":"'+ document.getElementById('catBreed').value +'" , "age":"'+ document.getElementById('age').value +'" , "weight":"'+ document.getElementById('weight').value +'" , "gender":"'+ document.getElementById('gender').value +'" , "height":"'+ document.getElementById('height').value +'" } ]}';
-					
+					'{ "question":"' + questionSelected + '" , "region":"'+ document.getElementById('region').value +'" , "catBreed":"'+ document.getElementById('catBreed').value +'" , "age":"'+ document.getElementById('age').value +'" , "weight":"'+ document.getElementById('weight').value +'" , "gender":"'+ document.getElementById('gender').value +'" , "height":"'+ document.getElementById('height').value +'" } ]}';
+		
+
+		if (questionSelected == "question0"){
+			alert("Please enter a question from the drop-down menu.");
+			return;
+		}
+				
 		// Convert from String to JSON Object
    		var jsonObj = JSON.parse(input);
 		
-		// Display JSON object on the screen 
+		// Display JSON object on the screen - Demo purposes only
 		document.getElementById('showResult2').innerHTML = jsonObj.interface[0].question + " " + jsonObj.interface[0].region + " " + jsonObj.interface[0].catBreed + " " + jsonObj.interface[0].age + " " + jsonObj.interface[0].weight + " " + jsonObj.interface[0].gender + " " + jsonObj.interface[0].height;
 	}
