c2e756e31f4dcdc61a27c4d4feb772df4d11a935
Alexander Green <agreen10@deadpool.socs.uoguelph.ca>
2016-11-01 10:55:13 -0400

    Made 'generateQuery function in backendInteraction.js, as well as a spec to test it in backendInteractionSpec.js

--- src/js/backendInteraction.js
+++ src/js/backendInteraction.js
@@ -7,6 +7,11 @@ function helloWorld() {
     return "Hello world!";
 }
 
+function jsonToString(input){
+	//return "region1 cat1 breed1 age1 weight1 gender1 height1";
+    return input.interface[0].region + " " + input.interface[0].catBreed + " " + input.interface[0].age + " " + input.interface[0].weight + " " + input.interface[0].gender + " " + input.interface[0].height;
+}
+
 /*-------
     Purpose: Create an SQL query from a json object
     Parameters: input, a json object
@@ -25,116 +30,116 @@ function generateQuery(input){
     var queryString;
 
     if(input){
-        switch (input.region) {
+        switch (input.interface[0].region) {
             case "region0":
                 region = "IS NOT NULL";
                 break;
             case "region1":
-                region = " = 'Canada'";
+                region = '= "Canada"';
                 break;
             case "region2":
-                region = " = 'United States'";
+                region = '= "United States"';
                 break;
         }
 
-        switch (input.catBreed) {
+        switch (input.interface[0].catBreed) {
             case "catBreed0":
                 breed = "IS NOT NULL";
                 break;
             case "catBreed1":
-                breed = " = 'British Shorthair'";
+                breed = '= "British Shorthair"';
                 break;
             case "catBreed2":
-                breed = " = 'Siamese'";
+                breed = '= "Siamese"';
                 break;
             case "catBreed3":
-                breed = " = 'Persian'";
+                breed = '= "Persian"';
                 break;
             case "catBreed4":
-                breed = " = 'Maine Coon'";
+                breed = '= "Maine Coon"';
                 break;
         }
 
-        switch (input.age) {
+        switch (input.interface[0].age) {
             case "age0":
                 lowerAge = "IS NOT NULL";
                 upperAge = "IS NOT NULL";
                 break;
             case "age1":
                 lowerAge = "IS NOT NULL";
-                upperAge = " < 1";
+                upperAge = "< 1";
                 break;
             case "age2":
-                lowerAge = " >= 1";
-                upperAge = " <= 2";
+                lowerAge = ">= 1";
+                upperAge = "<= 2";
                 break;
             case "age3":
-                lowerAge = " >= 3";
-                upperAge = " <= 6";
+                lowerAge = ">= 3";
+                upperAge = "<= 6";
                 break;
             case "age4":
-                lowerAge = " > 6";
+                lowerAge = "> 6";
                 upperAge = "IS NOT NULL";
                 break;
         }
 
-        switch (input.weight) {
+        switch (input.interface[0].weight) {
             case "weight0":
                 lowerWeight = "IS NOT NULL";
                 upperWeight = "IS NOT NULL";
                 break;
             case "weight1":
                 lowerWeight = "IS NOT NULL";
-                upperWeight = " <= 2";
+                upperWeight = "<= 2";
                 break;
             case "weight2":
-                lowerWeight = " >= 3";
-                upperWeight = " <= 4";
+                lowerWeight = ">= 3";
+                upperWeight = "<= 4";
                 break;
             case "weight3":
-                lowerWeight = " >= 5";
-                upperWeight = " <= 10";
+                lowerWeight = ">= 5";
+                upperWeight = "<= 10";
                 break;
             case "weight4":
-                lowerWeight = " > 10";
+                lowerWeight = "> 10";
                 upperWeight = "IS NOT NULL";
                 break;
         }
 
-        switch (input.gender) {
+        switch (input.interface[0].gender) {
             case "gender0":
                 gender = "IS NOT NULL";
                 break;
             case "gender1":
-                gender = "= 'Male'";
+                gender = '= "Male"';
                 break;
             case "gender2":
-                gender = "= 'Female'";
+                gender = '= "Female"';
                 break;
             case "gender3":
-                gender = "= 'Other'";
+                gender = '= "Other"';
                 break;
         }
 
-        switch (input.height) {
+        switch (input.interface[0].height) {
             case "height0":
                 lowerHeight = "IS NOT NULL";
                 upperHeight = "IS NOT NULL";
                 break;
             case "height1":
-                lowerHeight = " >= 1";
-                upperHeight = " <= 10";
+                lowerHeight = ">= 1";
+                upperHeight = "<= 10";
                 break;
             case "height2":
-                lowerHeight = " >= 11";
-                upperHeight = " <= 20";
+                lowerHeight = ">= 11";
+                upperHeight = "<= 20";
                 break;
             case "height3":
-                lowerHeight = " >= 21";
-                upperHeight = " <= 30";
+                lowerHeight = ">= 21";
+                upperHeight = "<= 30";
                 break;
             case "height4":
-                lowerHeight = " > 30";
+                lowerHeight = "> 30";
                 upperHeight = "IS NOT NULL";
                 break;
         }
--- testCases/backendInteractionSpec.js
+++ testCases/backendInteractionSpec.js
@@ -3,3 +3,26 @@ describe("Backend Interaction Subsystem", function() {
     expect(helloWorld()).toEqual("Hello world!");
   });
 });
+
+describe("Generate sql query", function() {
+  var inputString = '{' +
+                       '"interface":[{' +
+                         '"region": "region1",' +
+                         '"catBreed": "catBreed1",' +
+                         '"age": "age1",' +
+                         '"weight": "weight1",' +
+                         '"gender": "gender1",' +
+                         '"height": "height1"' +
+                       '}]' +
+                    '}';
+  var input = JSON.parse(inputString);
+
+  it("Takes a json object with the expected fields", function() {
+    expect(jsonToString(input)).toEqual("region1 catBreed1 age1 weight1 gender1 height1");
+  });
+
+  it("Generates sql query from json object", function() {
+    expect(generateQuery(input)).toEqual('SELECT * FROM Cats WHERE region = "Canada" AND catBreed = "British Shorthair" AND age IS NOT NULL AND age < 1' +
+                      ' AND weight IS NOT NULL AND weight <= 2 AND gender = "Male" AND height >= 1 AND height <= 10');
+  });
+});
