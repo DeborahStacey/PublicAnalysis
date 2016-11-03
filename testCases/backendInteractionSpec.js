6351327835951ce690ba65550306e47bb654268b
Vincent Yong <yong_vince850@yahoo.ca>
2016-11-03 10:24:18 -0400

    trying to get jquery working. haven't broken through yet

--- testCases/backendInteractionSpec.js
+++ testCases/backendInteractionSpec.js
@@ -13,11 +13,13 @@ describe("Test Connection to databases", function() {
 
   var connectionstring="Data Source=welcat-working.cz3evk2oqbiz.us-west-2.rds.amazonaws.com;Initial Catalog=WelcatWorking; User ID=wellcatreaders2;Password=thatbackendtho;Provider=SQLOLEDB";  
 
+  console.log("test test");
+
   jQuery.ajax({
     type: "POST",
     url: 'connection.php',
     dataType: 'json',
-    data: {functionname: 'add', arguments: [1, 2]},
+    data: {functionname: 'test'},
 
     success: function (obj, textstatus) {
       if( !('error' in obj) ) {
@@ -26,6 +28,11 @@ describe("Test Connection to databases", function() {
       else {
           console.log(obj.error);
       }
+
+      console.log(obj.error);
+    }
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus, errorThrown);
     }
   });
 
--- testCases/connection.php
+++ testCases/connection.php
@@ -1,28 +1,26 @@
 <?php
     header('Content-Type: application/json');
 
+
     $aResult = array();
 
     if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
 
-    if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
+    //if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
 
     if( !isset($aResult['error']) ) {
 
-        switch($_POST['functionname']) {
-            case 'add':
-               if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
-                   $aResult['error'] = 'Error in arguments!';
-               }
-               else {
-                   $aResult['result'] = add(floatval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
-               }
-               break;
+      switch($_POST['functionname']) {
+        case 'test':
+          $conn_string = "host=sheep port=5432 dbname=test user=lamb password=bar";
+          //$dbconn4 = pg_connect($conn_string);
+          $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+        break;
 
-            default:
-               $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
-               break;
-        }
+        default:
+          $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+        break;
+      }
 
     }
 
