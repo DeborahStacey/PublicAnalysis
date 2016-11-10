8153d6c98dfa23da8d04d8a29403d6aa805bff01
Vincent Yong <yong_vince850@yahoo.ca>
2016-11-10 09:56:52 -0500

    python script added in to test

---
+++ testCases/testConnection.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import psycopg2
+import sys
+ 
+host = 'welcat-working.cz3evk2oqbiz.us-west-2.rds.amazonaws.com'
+dbname = 'WelcatWorking'
+user = 'wellcatreaders2'
+password = 'thatbackendtho'
+port = 5432
+ 
+conn_string = "host='%s' dbname='%s' user='%s' password='%s' port='%i'"\
+                % (host, dbname, user, password, port)
+ 
+# print the connection string we will use to connect
+print "Connecting to database\n ->%s" % (conn_string)
+ 
+try:
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+    print "Connected!\n"
+except:
+    # Get the most recent exception
+    exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+    # Exit the script and print an error telling what happened.
+    sys.exit("Database connection failed!\n ->%s" % (exceptionValue))
