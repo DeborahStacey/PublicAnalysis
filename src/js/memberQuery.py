c27ca215dcf53f422efe744c1b4584d0d1de7033
Alexander Green <agreen10@uoguelph.ca>
2016-11-17 10:45:46 -0500

    Removed 'print jsonInfo[]' line from memberQuery.py, and changed 'print row[0]' to 'print row[1]'

--- src/js/memberQuery.py
+++ src/js/memberQuery.py
@@ -5,10 +5,10 @@ import json
 
 jsonInfo = json.load(sys.stdin).iteritems()
 
-print jsonInfo[]
+print jsonInfo
 
 for row in jsonInfo:
-	print row[0]
+	print row[1]
 
 #print jsonInfo
 #'{"region": "region1", "catBreed": "catBreed1", "age": "age1", "weight": "weight1", "gender": "gender1", "height": "height1"}'
