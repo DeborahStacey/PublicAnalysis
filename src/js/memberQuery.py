047acd40a63b05da73e88b7fa3392c7876bafc4c
Vincent Yong <yong_vince850@yahoo.ca>
2016-11-17 10:35:32 -0500

    rearranged code in memberQuery.py

--- src/js/memberQuery.py
+++ src/js/memberQuery.py
@@ -11,4 +11,4 @@ for row in jsonInfo:
 	print row[0]
 
 #print jsonInfo
-#'{"interface":[{"region": "region1", "catBreed": "catBreed1", "age": "age1", "weight": "weight1", "gender": "gender1", "height": "height1"}] }'
+#'{"region": "region1", "catBreed": "catBreed1", "age": "age1", "weight": "weight1", "gender": "gender1", "height": "height1"}'
